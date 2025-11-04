01f9d143f940643db56604e328496022a8469710
BlueSara97 <sarapb@stud.ntnu.no>
2025-11-04 13:42:08 +0100

    Creating PageHeadline, adding input field and design for the headline.

--- StudyGroup/app/src/main/java/components/PageHeadline.kt
+++ StudyGroup/app/src/main/java/components/PageHeadline.kt
@@ -1,2 +1,64 @@
 package components
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.studygroup.ui.theme.LocalCustomColors
+import com.example.studygroup.ui.theme.LocalSpacing
+
+@Composable
+fun PageHeadline(
+    text: String = "",
+) {
+    val colors = LocalCustomColors.current
+    val space = LocalSpacing.current
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(
+                width = 1.dp,
+                color = colors.borderColor,
+                shape = RectangleShape
+            )
+            .padding(horizontal = space.m, vertical = space.xs)
+            .then(
+                Modifier.drawBehind {
+                    val strokeWidth = 1.dp.toPx()
+                    val y = size.height - strokeWidth / 2
+                    drawLine(
+                        color = colors.borderColor,
+                        start = Offset(0f, y),
+                        end = Offset(size.width, y),
+                        strokeWidth = strokeWidth
+                    )
+                }
+            )
+    ) {
+        Text(
+            text = text,
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(space.xs)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewPageHeadLine(){
+    PageHeadline(text = "hhahahahah")
+}
