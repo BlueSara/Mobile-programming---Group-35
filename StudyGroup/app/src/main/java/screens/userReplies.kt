c6f86e218ca16a7a35a969d4a59b4099c6e3782b
isakeh <150025599+isakeghv@users.noreply.github.com>
2025-11-23 23:05:16 +0100

    Updated screen so components are directly in screen, and minimized it a bit
    set so "cardSmall" has ".lowerCase" when checking response to make it not case-sensitive

--- StudyGroup/app/src/main/java/screens/userReplies.kt
+++ StudyGroup/app/src/main/java/screens/userReplies.kt
@@ -1,26 +1,20 @@
 package screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateMapOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import components.PageHeadline
 import components.cards.CardSmall
-import components.icons.ChatBubble
 import components.icons.EditIcon
 import layout.Layout
-import kotlin.collections.set
-import androidx.compose.runtime.toMutableStateMap
+import com.example.studygroup.ui.theme.LocalSpacing
 import components.AppButton
 import components.ButtonType
 import components.PopUp
@@ -28,7 +22,7 @@ import components.PopUp
 val mockdata: MutableList<MutableMap<String, Any>> = mutableListOf(
     mutableMapOf(
         "title" to "tittel 1",
-        "postID" to "A7tTGFUUO1JCqqIw1Tph",
+        "postID" to "A7tTGFUUO1JCqqIw1Tphfdf",
         "subject" to  "avansert",
         "subjectCode" to "PROG2006",
         "expirationDate" to "",
@@ -37,7 +31,7 @@ val mockdata: MutableList<MutableMap<String, Any>> = mutableListOf(
     ),
     mutableMapOf(
         "title" to "asfdasdfasdfasdf",
-        "postID" to "KUB7PYq8QAVum7LP2G2f",
+        "postID" to "KUB7PYq8QAVum7LP2G2fgtha",
         "subject" to  "objektorientert programmering",
         "subjectCode" to "PROG1003",
         "expirationDate" to "",
@@ -46,7 +40,7 @@ val mockdata: MutableList<MutableMap<String, Any>> = mutableListOf(
     ),
     mutableMapOf(
         "title" to "trenger hjelp med mobil asap zulu plis :((",
-        "postID" to "KUB7PYq8QAVum7LP2G2f",
+        "postID" to "KUB7PYq8QAVum7LP2G2fhsac",
         "subject" to  "objektorientert programmering",
         "subjectCode" to "PROG2007",
         "expirationDate" to "",
@@ -55,7 +49,7 @@ val mockdata: MutableList<MutableMap<String, Any>> = mutableListOf(
     ),
     mutableMapOf(
         "title" to "tittel 2",
-        "postID" to "KUB7PYq8QAVum7LP2G2f",
+        "postID" to "KUB7PYq8QAVum7LP2G2fgdjya",
         "subject" to  "objektorientert programmering",
         "subjectCode" to "PROG1003",
         "expirationDate" to "",
@@ -64,13 +58,19 @@ val mockdata: MutableList<MutableMap<String, Any>> = mutableListOf(
     ),
     mutableMapOf(
         "title" to "tittel 2",
-        "postID" to "KUB7PYq8QAVum7LP2G2f",
+        "postID" to "KUB7PYq8QAVum7LP2G2fgj533",
         "subject" to  "objektorientert programmering",
         "subjectCode" to "PROG1003",
         "expirationDate" to "",
         "topic" to "need help pls",
         "response" to "skip"
     ),
+)
+//used to iterate over to create AppButtons instead of having to manually create each button
+val buttonOptions = listOf(
+    listOf("Skip", ButtonType.DANGER),
+    listOf("Ditto", ButtonType.PRIMARY),
+    listOf("Assist", ButtonType.SUCCESS),
 )
 
 /**
@@ -81,122 +81,69 @@ val mockdata: MutableList<MutableMap<String, Any>> = mutableListOf(
 @Preview(showBackground = true)
 fun UserReplies(
     navController: NavHostController ?=null,){
+    val mSelectedPost = remember { mutableStateMapOf<String, Any>() }
+    var mAllPosts = remember { mutableListOf<Map<String, Any>>().toMutableStateList() }
+    val space = LocalSpacing.current
 
-    // TODO: actually get some data here from the API instead of mock data
+    /**Function calling api-handler to get posts*/
+    fun getPosts(){
+        // TODO : Get actual post data via function calling api instead of mockup
+        mAllPosts = mockdata.toMutableStateList()
+    }
 
+    /** Function calls the api, updates the response and UI
+     * @param response - String. No default value. The response that should be updated to
+     * */
+    fun handleUpdateResponse(response: String){
+        // TODO : add actual function-call to api-handler function and update response
+        val updateSuccess = true
+        val i = mAllPosts.indexOfFirst {it["postID"] == mSelectedPost["postID"]}
+        if (response == mSelectedPost["response"] || !updateSuccess || i == -1)  return
+
+        //updating the response in mSelectedPost, and updating the item in list (UI)
+        mSelectedPost["response"] = response.lowercase()
+        mAllPosts[i] = mSelectedPost.toMutableMap()
+    }
+    getPosts()
 
     Layout(
         navController= navController,
         arrowBack = false,
         pageDetails = { PageHeadline("Your Responses") },
         content = {
-
-            // TODO: replace mock data with actual data
-            mockdata.forEach {post ->
-                NewResponse(post)
-            }
-
-
-
-        }
-
-    )
-}
-
-/**
- * Creates a single new response to display to the user.
- * This is its own function because each response needs to store and update some state about itself,
- * which is very unpractical to do without a separate function.
- *
- * @param post the data to display
- */
-@Composable
-fun NewResponse(
-    post: MutableMap<String, Any>
-) {
-    // creates a mutable state from the post data
-    val mPost = remember { mutableStateMapOf<String, Any>().apply { putAll(post) } }
-    val mShowPopup = remember { mutableStateOf(false) }
-
-    CardSmall (
-        post = mPost,
-        onClick = {
-            mShowPopup.value = true
-        },
-        showResponse = true,
-        icon = { EditIcon() }
-    )
-
-    if (mShowPopup.value) {
-        PopUp (
-            onDismiss = { mShowPopup.value = false },
-            title = "Change your reply",
-            content = {
-                Row {
-                    AppButton(
-                        modifier = Modifier.weight(1f),
-                        type = ButtonType.DANGER,
-                        onClick = {
-
-                            // update value if we managed to successfully managed to upload it to the database
-                            val oldResponse = mPost["response"] as Any
-                            mPost["response"] = "skip"
-                            if (!tryUploadResponseToDatabase(mPost)) mPost["response"] = oldResponse
-                            mShowPopup.value = false },
-                        text = "Skip"
-                    )
-
-
-                    Spacer(Modifier.width(8.dp))
-
-
-                    AppButton(
-                        modifier = Modifier.weight(1f),
-                        type = ButtonType.PRIMARY,
-                        onClick = {
-                            val oldResponse = mPost["response"] as Any
-                            mPost["response"] = "ditto"
-                            if (!tryUploadResponseToDatabase(mPost)) mPost["response"] = oldResponse
-                            mShowPopup.value = false },
-                        text = "Ditto"
-                    )
-
-
-                    Spacer(Modifier.width(8.dp))
-
-
-                    AppButton(
-                        modifier = Modifier.weight(1f),
-                        type = ButtonType.SUCCESS,
-                        onClick = {
-                            val oldResponse = mPost["response"] as Any
-                            mPost["response"] = "assist"
-                            if (!tryUploadResponseToDatabase(mPost)) mPost["response"] = oldResponse
-                            mShowPopup.value = false
-                                  },
-                        text = "Assist"
+            Column(
+                verticalArrangement = Arrangement.spacedBy(space.m),
+            ) {
+                mAllPosts.forEach {post ->
+                    CardSmall (
+                        post = post,
+                        onClick = {mSelectedPost.putAll(post) },
+                        showResponse = true,
+                        icon = { EditIcon() }
                     )
                 }
             }
-        )
-    }
-
-    Spacer(Modifier.height(8.dp))
-}
-
-
-// try to upload the new response to the database
-fun tryUploadResponseToDatabase (
-    post: Map<String, Any>
-): Boolean {
-
-    // TODO: implement
-
-    // val result = tryUpdateDatabase(post)
-    //
-    // if (result != good) {
-    //      return false
-    // }
-
-    return true
+            if (!mSelectedPost.isEmpty()){
+                PopUp (
+                    onDismiss = { mSelectedPost.clear() },
+                    title = "Change your reply",
+                    content = {
+                        Row( horizontalArrangement = Arrangement.spacedBy(space.m) ) {
+                            buttonOptions.forEach { btn ->
+                                AppButton(
+                                    modifier = Modifier.weight(1f),
+                                    type = btn[1] as ButtonType,
+                                    text = btn[0].toString(),
+                                    onClick = {
+                                        handleUpdateResponse(response = btn[0].toString().lowercase())
+                                        mSelectedPost.clear()
+                                    }
+                                )
+                            }
+                        }
+                    }
+                )
+            }
+        }
+    )
 }
