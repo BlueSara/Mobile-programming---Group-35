6ea473f131cef471a3405c65b693618bd7d15b7d
isakeh <150025599+isakeghv@users.noreply.github.com>
2025-11-28 12:23:44 +0100

    implemented to messages get returned with correct content for the message-display to be "correct" without having to check it in front-end

--- api/utils/groups.go
+++ api/utils/groups.go
@@ -1,6 +1,9 @@
 package utils
 
-import "studygroup_api/models"
+import (
+	"slices"
+	"studygroup_api/models"
+)
 
 func CreateMembersNewGroup(responses []models.UserResponse) ([]string, []string, []string) {
 	var dittoUsers []string
@@ -18,4 +21,21 @@ func CreateMembersNewGroup(responses []models.UserResponse) ([]string, []string,
 	}
 
 	return dittoUsers, assistUsers, participants
+}
+
+func FindDecliningUsers(group models.Group, message models.Message, post models.Post, userID string) (bool, bool, bool) {
+	var assistAgreed bool
+
+	userAgreed := slices.Contains(message.UsersAgreed, userID)
+	ownerAgreed := slices.Contains(message.UsersAgreed, *post.UserID)
+
+	for _, a := range group.AssistingUsers {
+		assistAgreed = slices.Contains(message.UsersAgreed, a)
+	}
+
+	return userAgreed, ownerAgreed, assistAgreed
+}
+
+func MeetingIsSelected(ownerAgreed, assistAgreed bool, group models.Group, message models.Message) bool {
+	return len(group.Participants)/2 >= len(message.UsersAgreed) && ownerAgreed && assistAgreed
 }
