a5f6dd470b3928749fd37a708fda18317e26a34c
marta <martafg@stud.ntnu.no>
2025-11-29 18:31:20 +0100

    Show connectivity status banner in Compose

--- StudyGroup/app/src/main/java/utils/ConnectivityObserver.kt
+++ StudyGroup/app/src/main/java/utils/ConnectivityObserver.kt
@@ -2,5 +2,9 @@ package utils
 
 // Gives feedback on if we're connected to a network
 interface ConnectivityObserver{
-    fun observe(): kotlinx.coroutines.flow.Flow<Boolean>
+    fun observe(): kotlinx.coroutines.flow.Flow<Status>
+
+    enum class Status{
+        Losing, Lost, Available, Unavailable
+    }
 }
