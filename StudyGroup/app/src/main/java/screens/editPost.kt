66ffa961316088c39ebce8b4c10312ea63c25441
BlueSara97 <sarapb@stud.ntnu.no>
2025-12-06 17:36:58 +0100

    Finishing using real data instead of mocked in: userposts, editpost and createpost.

--- StudyGroup/app/src/main/java/screens/editPost.kt
+++ StudyGroup/app/src/main/java/screens/editPost.kt
@@ -1,23 +1,28 @@
 package screens
 
+import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.semantics.Role
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.studygroup.ui.theme.LocalSpacing
@@ -27,44 +32,160 @@ import components.DateInput
 import components.PageHeadline
 import components.Select
 import components.TextArea
+import components.TextInput
+import handleApiReqGet
+import handleApiReqPost
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import layout.Layout
 import java.time.LocalDate
+import dataLayer.firebase.UniversityService
+import dataLayer.firebase.Subject
+import handleApiReqDelete
+import handleApiReqPut
 
-@Preview(showBackground = true)
 @Composable
 fun EditPost(
-    navController: NavHostController ?=null,) {
-    val availableSubjects = mockUpSubjects // TODO: USE REAL DATA
-    var mSelectedSubject by remember { mutableStateOf("") }
-    var mTopic by remember { mutableStateOf("") }
-    var mExpirationDate by remember { mutableStateOf(LocalDate.now()) }
-    var mUseLocation by remember { mutableStateOf(true) }
-
+    navController: NavHostController? = null,
+    postID: String?
+) {
     val space = LocalSpacing.current
+    val context = LocalContext.current
 
-    fun handlePushingPost(){
-        //TODO : ADD LOGIC TO VALIDATE AND PUBLISH POST (API)
+    // post data from the  API
+    var postData by remember { mutableStateOf<Map<String, Any>?>(null) }
+
+    // Subjects from Firestorehandler
+    val universityService = remember { UniversityService() }
+    var subjects by remember { mutableStateOf(listOf<Subject>()) }
+
+    var mSelectedSubject by remember { mutableStateOf("") }
+    var mSelectedSubjectName by remember { mutableStateOf("") }
+    var mTopic by remember { mutableStateOf("") }
+    var mDescription by remember { mutableStateOf("") }
+    var mExpirationDate by remember { mutableStateOf(LocalDate.now()) }
+    var mUseLocation by remember { mutableStateOf(false) }
+
+    // Get all the subjects from firestore
+    LaunchedEffect(Unit) {
+        try {
+            val result = universityService.getAllSubjects()
+            subjects = result
+        } catch (e: Exception) {
+            Log.e("EDIT_POST", "Failed to load subjects: $e")
+        }
     }
 
+    // Get the data of post
+    LaunchedEffect(postID) {
+        if (postID != null) {
+            val res = handleApiReqGet("/posts/$postID", context)
+            if (res.ok) {
+                postData = res.content as? Map<String, Any>
+                Log.e("EDIT_POST", "loaded: $postData")
+            } else {
+                Log.e("EDIT_POST", "Failed to load post: ${res.code} | ${res.content}")
+            }
+        }
+    }
+
+    // update the new data.
+    LaunchedEffect(postData) {
+        postData?.let { post ->
+            mSelectedSubject = post["SubjectID"]?.toString() ?: ""
+            mSelectedSubjectName = post["Subject"]?.toString() ?: ""
+            mTopic = post["Title"]?.toString() ?: ""
+            mDescription = post["Topic"]?.toString() ?: ""
+            mUseLocation = post["UseProximity"] as? Boolean ?: false
+
+            val dateRaw = post["ExpirationDate"]?.toString() ?: ""
+            if (dateRaw.length >= 10) {
+                mExpirationDate = LocalDate.parse(dateRaw.substring(0, 10))
+            }
+        }
+    }
+
+    fun handleUpdatePost() {
+        if (postID == null) return
+
+        val formattedDate = "${mExpirationDate}T00:00:00Z"
+
+        val body = mapOf(
+            "title" to mTopic,
+            "subjectID" to mSelectedSubject,   // firestoreID
+            "topic" to mDescription,
+            "useProximity" to mUseLocation,
+            "expirationDate" to formattedDate
+        )
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val res = handleApiReqPut("/posts/$postID", context, body)
+
+            if (res.ok) {
+                Log.d("EDIT_POST", "Updated successfully")
+                withContext(Dispatchers.Main) {
+                    navController?.navigate("userPost")
+                }
+            } else {
+                Log.e("EDIT_POST", "Update FAILED: ${res.code} | ${res.content}")
+            }
+        }
+    }
+
+    fun handleDeletePost() {
+       if (postID == null) return
+
+       CoroutineScope(Dispatchers.IO).launch {
+           val res = handleApiReqDelete("/post/$postID", context)
+
+           if (res.ok) {
+               Log.d("EDIT_POST", "Delete successfull!")
+               withContext(Dispatchers.Main) {
+                   navController?.navigate("userPost")
+               }
+           } else {
+               Log.e("EDIT_POST", "DELETE FAIL :( ${res.code} | ${res.content}")
+           }
+       }
+    }
 
     Layout(
-        navController= navController,
+        navController = navController,
         showFilter = true,
         pageDetails = { PageHeadline(text = "Edit post") },
-        content =  {
+        content = {
             Column(
-                modifier = Modifier
-                    .fillMaxWidth()
+                modifier = Modifier.fillMaxWidth()
             ) {
                 Text("Subject", fontSize = 20.sp)
+
                 Select(
-                    onChange = { mSelectedSubject = it },
-                    options = availableSubjects
+                    onChange = { selectedId ->
+                        mSelectedSubject = selectedId
+                        val obj = subjects.find { it.firestoreID == selectedId }
+                        mSelectedSubjectName = obj?.name ?: ""
+                    },
+                    options = subjects.map { subject ->
+                        mapOf(
+                            "id" to (subject.firestoreID ?: ""),
+                            "name" to ("${subject.code} - ${subject.name}")
+                        )
+                    }
                 )
-                TextArea(
+
+                TextInput(
                     value = mTopic,
                     onChange = { mTopic = it },
-                    label = "Assignment/topic")
+                    label = "Assignment/topic"
+                )
+
+                TextArea(
+                    value = mDescription,
+                    onChange = { mDescription = it },
+                    label = "Description"
+                )
 
                 DateInput(
                     date = mExpirationDate,
@@ -77,13 +198,11 @@ fun EditPost(
                     horizontalArrangement = Arrangement.spacedBy(space.s),
                     modifier = Modifier
                         .fillMaxWidth()
-                        // make entire row clickable so users may tap the label
                         .clickable(
                             onClick = { mUseLocation = !mUseLocation },
                             role = Role.Checkbox
                         )
                 ) {
-
                     Checkbox(
                         modifier = Modifier
                             .width(space.m)
@@ -92,16 +211,28 @@ fun EditPost(
                         onCheckedChange = { mUseLocation = !mUseLocation },
                     )
 
-                    Text(text = "Visible only to those close to your location",fontSize = 18.sp)
+                    Text(
+                        text = "Visible only to those close to your location",
+                        fontSize = 18.sp
+                    )
+                }
+                Box(
+                    modifier = Modifier.padding(top=space.xl*2)
+                ){
+
+                    AppButton(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = "Delete post",
+                        type = ButtonType.DANGER,
+                        onClick = { handleDeletePost() }
+                    )
                 }
             }
-
 
         },
         footer = {
             Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.spacedBy(space.xs)
             ) {
                 AppButton(
@@ -113,7 +244,7 @@ fun EditPost(
                 AppButton(
                     modifier = Modifier.weight(1f),
                     text = "Publish",
-                    onClick = { handlePushingPost() }
+                    onClick = { handleUpdatePost() }
                 )
             }
         }
--- StudyGroup/app/src/main/java/screens/userPosts.kt
+++ StudyGroup/app/src/main/java/screens/userPosts.kt
@@ -1,45 +1,65 @@
 package screens
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import com.example.studygroup.ui.theme.LocalSpacing
 import components.PageHeadline
+import components.TextInput
 import components.cards.CardSmall
+import components.icons.EditIcon
+import handleApiReqGet
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import layout.Layout
 
-val postsMockups = mutableListOf<Map<String, Any>>(
-    mapOf(
-        "title" to "POST 1, some topic about this post to get help with",
-        "topic" to "topic 1",
-        "subjectCode" to "prog2007",
-        "subject" to "Mobile programming",
-        "expirationDate" to "29-11-2025"
-    ),
-    mapOf(
-        "title" to "POST 2, some topic about this post to get help with",
-        "topic" to "topic 2",
-        "subjectCode" to "prog2007",
-        "subject" to "Mobile programming",
-        "expirationDate" to "29-11-2025"
-    ),
-    mapOf(
-        "title" to "POST 3, some topic about this post to get help with",
-        "topic" to "topic 3",
-        "subjectCode" to "prog2007",
-        "subject" to "Mobile programming",
-        "expirationDate" to "29-11-2025"
-    )
-)
 @Composable
 fun UserPosts(
     navController: NavHostController?= null
 ) {
 
     val space = LocalSpacing.current
+    val context = LocalContext.current
 
+    // Holds all posts created by the user
+    val mUserPosts = remember { mutableStateListOf<Map<String,Any>>() }
+
+    LaunchedEffect(Unit) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val res = handleApiReqGet("/posts/own", context)
+
+            if(res.ok) {
+
+                val raw = res.content as? List<Map<String, Any>> ?: emptyList()
+
+                val list = raw.map { post ->
+                    mapOf(
+                        "postID" to post["PostID"],
+                        "title" to post["Title"],
+                        "topic" to post["Topic"],
+                        "subject" to post["Subject"],
+                        "subjectCode" to post["SubjectCode"],
+                        "expirationDate" to post["ExpirationDate"]
+                    )
+                }
+                mUserPosts.clear()
+                mUserPosts.addAll(list as Collection<Map<String, Any>>)
+
+                Log.e("USER_POSTS", "load: ${res.code} | ${res.content}")
+            } else {
+                Log.e("USER_POSTS", "failed load: ${res.code} | ${res.content}")
+            }
+        }
+    }
     Layout(
         navController = navController,
         arrowBack = false,
@@ -50,14 +70,19 @@ fun UserPosts(
             Column(
                 verticalArrangement = Arrangement.spacedBy(space.s),
             ) {
-                // TODO: Have to use real data
-                postsMockups.forEach { post ->
+                if (mUserPosts.isEmpty())
+                    TextInput(
+                        value = "You have no posts yet!"
+                    )
+                mUserPosts.forEach { post ->
                     CardSmall(
                         post = post,
                         onClick = {
-                            // TODO: Navigate to the clicked post
-                            // TODO: navController?.navigate("postDetails/${post["postID"]}
-                        }
+                            val id = post["postID"].toString()
+                            navController?.navigate("editPost/$id")
+
+                        },
+                        icon = { EditIcon() }
                     )
                 }
             }
